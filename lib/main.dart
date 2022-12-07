f68f87c1e9059fd99b722435660f14d4cc5c5288
asdfghjkl123 <iskandarmister323@gmail.com>
2022-12-07 18:30:36 +0000

    test listtile created

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      color: Colors.grey.shade500,
+      home: Scaffold(
+          appBar: AppBar(title: const Center(child: Text("Conatainer"))),
+          body: Center(
+            child: Column(children: [
+              Padding(
+                  padding: EdgeInsets.only(left: 2.0, top: 1.0, right: 2.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1.0)),
+                    child: ListTile(
+                      leading: Icon(Icons.add_card),
+                      title: const Text("dasdadsad"),
+                      subtitle: const Text("sfdsfsfdsfdgfdg"),
+                    ),
+                  ))
+            ]),
+          )),
     );
   }
 }
